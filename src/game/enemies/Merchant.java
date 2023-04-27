76dada9cca5b6fae9a5da0322b04ce154334313c
gsin0055 <gsin0055@student.monash.edu>
2023-04-28 00:35:01 +1000

    removing merchant

--- src/game/enemies/Merchant.java
+++
@@ -1,9 +0,0 @@
-package game.enemies;
-
-public class Merchant extends Enemy {
-
-    public Merchant(String name, char displayChar, int hp) {
-        super("Trader Kale", 'k' , 100000);
-    }
-
-}
